6be72b6c18201c82aa0195325d159c7f61477e2f
Tyler Flynn <tyler94flynn@gmail.com>
2022-07-30 11:25:37 -0400

    seeds added

--- schema.sql
+++ schema.sql
@@ -30,3 +30,44 @@ CREATE TABLE employee (
   FOREIGN KEY (manager_id) REFERENCES employee(id)
 
 );
+
+--seeds
+----departments
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
+----roles
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 190000, 4);
+----employees
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jessica", "Haze", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Tiffany", "Patric", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Mia","Lam",null,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Bently", "Lao", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Chris", "Melby", 4, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jason", "Baker", 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Tom", "Nice", 2, 7);
