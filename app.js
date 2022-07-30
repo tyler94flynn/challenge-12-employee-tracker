91ab320f552a6f50f43fd184734f1414abed8f6d
Tyler Flynn <tyler94flynn@gmail.com>
2022-07-30 12:23:21 -0400

    fixed issue with mysql password

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "4Helloworld7!",
+    password: "Password123!",
     database: "employee_trackerDB"
 });
 
